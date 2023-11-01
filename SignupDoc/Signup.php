12f2608f2083e1b481320b85d8701500f09ceb75
keyvan <keyvan.akbarzadeh.2001@gmail.com>
2023-11-01 12:51:32 +0330

    add a Test commit

--- SignupDoc/Signup.php
+++ SignupDoc/Signup.php
@@ -1,12 +1,19 @@
 <?php
-//information's about doctors
-$fname = $_POST[""];
-$lname = $_POST[""];
-$number = $_POST[""];
-$city = $_POST[""];
-$clinicAddress = $_POST[""];
-$major = $_POST[""];
-$Expertise = $_POST[""];
-$emailAddress = $_POST[""];
-$gender = $_POST[""];
+// header("contenttype");
+// header("requestmethodPOST");
+// header("originx*");
 
+
+//information's about doctors
+$fname = $_POST[""];    //validate name without any number  
+$lname = $_POST[""];    //validate name without any number 
+$fileNumber = $_POST[""]; //validate شماره پرونده
+$number = $_POST[""];   //validate number without any char
+$city = $_POST[""];     //just peice of chars
+$clinicAddress = $_POST[""];    //just peice of chars
+$major = $_POST[""];    //just peice of chars
+$Expertise = $_POST[""];    //just peice of chars
+$emailAddress = $_POST[""];     //validate email with filter 
+$gender = $_POST[""];   //just peice of chars
+
+echo "hello world";
