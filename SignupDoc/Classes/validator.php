efe763df8895d0452f2a62bb88768dc6c664dae6
keyvan <keyvan.akbarzadeh.2001@gmail.com>
2023-11-10 16:57:38 +0330

    validator created

---
+++ SignupDoc/Classes/validator.php
@@ -0,0 +1,177 @@
+<?php
+class validator{
+    //properties
+    public $fname;
+    public $lname;
+    public $fileNumber;
+    public $number;
+    public $city;
+    public $clinikAddress;
+    public $major;
+    public $expertise;
+    public $emailAddress;
+    public $gender;
+
+
+
+
+    //function's
+    public function __construct(array $input){
+        $this->fname = $input[0];
+        $this->lname = $input[1];
+        $this->fileNumber = $input[2];
+        $this->number = $input[3];
+        $this->city = $input[4];
+        $this->clinikAddress = $input[5];
+        $this->major = $input[6];
+        $this->expertise = $input[7];
+        $this->emailAddress = $input[8];
+        $this->gender = $input[9];
+    }
+
+
+
+
+    
+    public function isEmpty(){
+        if ( empty($this->fname) || empty($this->lname) || empty($this->fileNumber) || empty($this->number) || empty($this->city) || empty($this->clinikAddress) || empty($this->major) || empty($this->expertise) || empty($this->emailAddress) || empty($this->gender) ) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+
+
+
+
+
+
+
+    // proper name without number and having suitable amount of char 3-31
+    public function validName(){
+        $fname = str_split($this->fname);
+        $lname = str_split($this->lname);
+        $isNumeric = false;
+        $numArray = [1,2,3,4,5,6,7,8,9];
+        for ($i=0; $i <count($fname) ; $i++) { 
+            foreach ($numArray as $value) {
+                if ($value == $fname[$i]) {
+                    $isNumeric = true;
+                    break;
+                }
+            }
+        }
+
+        for ($j=0; $j <count($lname) ; $j++) { 
+            foreach ($numArray as $value) {
+                if ($value == $lname[$j]) {
+                    $isNumeric = true;
+                    break;
+                }
+            }
+        }
+        if ((array_key_last($fname) > 31) || (array_key_last($lname) > 31) || ($isNumeric == true) || (array_key_last($fname)<2) ||(array_key_last($lname)<2)) {
+            return false; 
+        }else{
+            return true;
+        }
+    } 
+
+
+
+
+
+    // certain amount of char and number's
+    public function validFileNumber(){
+        str_replace(" ","",$this->fileNumber);
+        $fileNumber = str_split($this->fileNumber);
+
+        if (count($fileNumber) !== 10) {
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+
+
+
+
+    // phone number must be 11 char and start with 09 and not contain anything rather than nubmer's 
+    public function validPhoneNumber(){
+        $phoneNumber = str_split($this->number);
+        $numArray = [0,1,2,3,4,5,6,7,8,9];
+        for ($i=0; $i <11 ; $i++) { 
+            if(!in_array($phoneNumber[$i],$numArray)){
+                $isChar = true;
+                break;             
+            }
+        }
+        if ($phoneNumber[0] != 0 || $phoneNumber[1] != 9 || count($phoneNumber) !== 11) {
+            return false;
+        }
+        return true;
+
+    } 
+
+
+
+
+    //valid city that user have to choose in the list 
+    public function city(){
+        $city = file("Document/city.txt");
+        $lowerCity = strtolower($this->city);
+        $cityArray = array();
+        foreach ($city as $value) {
+            $holder = trim($value);
+            array_push($cityArray,strtolower($holder));
+        }
+        if (!in_array($lowerCity,$cityArray)) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+    //valid major in medical fields that use have to choose in the list
+    public function validmajor(){
+        $major = file("Document/major.txt");
+        $lowerMajor = strtolower($this->major);
+        $majorArray = array();
+        foreach ($major as $value) {
+            $holder = trim($value);
+            array_push($majorArray,strtolower($holder));
+        }
+        if (!in_array($lowerMajor,$majorArray)) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+
+    public function validEmail(){
+        if (filter_var($this->emailAddress,FILTER_VALIDATE_EMAIL) == false) {
+            return false;
+        }
+        return true;
+        
+    }
+
+
+
+
+    public function gender(){
+
+        if ($this->gender ==  "male" || $this->gender == "female" ) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
--- SignupDoc/Signup.php
+++ SignupDoc/Signup.php
@@ -3,16 +3,54 @@
 // header("requestmethodPOST");
 // header("originx*");
 
-
+// file_get_contents("php://input"); //can be used
 //information's about doctors
-$fname = $_POST[""];    //validate name without any number  
-$lname = $_POST[""];    //validate name without any number 
-$fileNumber = $_POST[""]; //validate شماره پرونده
-$number = $_POST[""];   //validate number without any char
-$city = $_POST[""];     //just peice of chars
-$clinicAddress = $_POST[""];    //just peice of chars
-$major = $_POST[""];    //just peice of chars
-$Expertise = $_POST[""];    //just peice of chars
-$emailAddress = $_POST[""];     //validate email with filter 
-$gender = $_POST[""];   //just peice of chars
+spl_autoload_register(function($className){
+    require_once "Classes/$className.php";
+});
 
+$doctorInfo = [
+0 =>$_POST["first"],    //validate name without any number   fname 
+1 =>$_POST["last"],    //validate name without any number   lanme
+// $username = $_POST[""];     //validate  #must have a seperate page 
+2 =>$_POST["fileNumber"], //validate شماره پرونده filenumber
+3 =>$_POST["phoneNumber"],   //validate number without any char    phonenumber
+4 =>$_POST["city"],     //just peice of chars   city
+5 =>$_POST["clinikAddress"],    //just peice of chars    clinik address
+6 =>$_POST["major"],    //just peice of chars    major
+7 =>$_POST["expertise"],    //just peice of chars    expertise
+8 =>$_POST["email"],     //validate email with filter    email
+9 =>$_POST["gender"]  //just peice of chars   gender 
+];
+
+
+// validate data 
+$doctor = new validator($doctorInfo);
+$array = get_class_methods($doctor);
+unset($array[0]); // delete __construct function from $array array
+$methods = array_values($array);
+$output = true;
+foreach ($methods as $value) {
+    $output = $doctor->$value();
+    if ($output == false) {
+        // error::class;
+        echo $value;
+        break;
+    }
+}
+
+
+
+// if ($output==true) {
+//     echo "test accepted!";
+//     //sanitize input data 
+//     // insert databse
+
+// }elseif($output == null){
+//     echo "test failed";
+// }
+// else{
+//     echo "test modified";
+// }
+
+//send user to fronpage 
